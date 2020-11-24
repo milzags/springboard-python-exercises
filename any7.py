9211b6971ab532e077d7284c5655feb98136408d
milanzagorac <zagoracm@gmail.com>
2020-11-24 16:28:44 -0500

    additional examples

--- any7.py
+++ any7.py
@@ -9,3 +9,19 @@ def any7(nums):
 print("should be true", any7([1, 2, 7, 4, 5]))
 print("should be false", any7([1, 2, 4, 5]))
 
+nums = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+
+evens = []
+for num in nums:
+    if num % 2 == 0:
+        evens.append(num)
+
+print(evens)
+# [2,4,6,8,10,12]
+
+# with list comprehension, this statement can be much shorter
+evens = [num for num in nums if num % 2== 0]
+
+doubled = [num * 2 for num in nums]
+
+#new list = [what we want, for each item in original list, #optional conditional statement]
