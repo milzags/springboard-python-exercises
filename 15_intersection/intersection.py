9b3fa20b829da12383a84105d22415fa001ba81a
milanzagorac <zagoracm@gmail.com>
2020-11-24 21:40:26 -0500

    more solutions

--- 15_intersection/intersection.py
+++ 15_intersection/intersection.py
@@ -13,3 +13,12 @@ def intersection(l1, l2):
         >>> intersection([1, 2, 3], [4, 5, 6])
         []
     """
+    intersect = set(l1).intersection(set(l2))
+    #other_method = l1 | l2
+    return list(intersect)
+
+# intersection is for sets so turn these lists into sets, find the intersection and then turn the list back into a set
+print(intersection([1, 2, 3], [2, 3, 4]))
+print(intersection([1, 2, 3], [1, 2, 3, 4]))
+print(intersection([1, 2, 3], [3, 4]))
+print(intersection([1, 2, 3], [4, 5, 6]))
