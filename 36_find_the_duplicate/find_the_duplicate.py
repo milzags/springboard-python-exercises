41e6ea0b094a6636a22681f5eddde6c7a94ff08f
milanzagorac <zagoracm@gmail.com>
2020-12-18 19:31:21 -0500

    solution

--- 36_find_the_duplicate/find_the_duplicate.py
+++ 36_find_the_duplicate/find_the_duplicate.py
@@ -13,3 +13,10 @@ def find_the_duplicate(nums):
         >>> find_the_duplicate([2, 1, 3, 4]) is None
         True
     """
+    checked_nums = set()
+    for num in nums:
+        if num in checked_nums:
+            return num
+        checked_nums.add(num)
+    
+print(find_the_duplicate([1, 2, 1, 4, 3, 12]))
